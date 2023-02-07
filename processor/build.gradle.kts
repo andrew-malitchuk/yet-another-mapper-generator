a2f35fc4104ac2cb7e22cdcd7ddbbad9f3dc7051
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-02-07 14:57:41 +0200

    YAMG-1 Migrate to build.gradle.kts (:processor)

---
+++ processor/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    namespace = "dev.yamg.processor"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.appCompat)
+    implementation(libs.coreKtx)
+    implementation(libs.material)
+}
