1b0b42d47efbab3909aef1f7b81ccbd8efffd2db
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-02-20 18:59:03 +0200

    YAMG-8 Add YamgItemVisitor

--- processor/src/main/kotlin/dev/yamg/processor/processing/YamgItemSymbolProcessor.kt
+++ processor/src/main/kotlin/dev/yamg/processor/processing/YamgItemSymbolProcessor.kt
@@ -5,6 +5,10 @@ import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSVisitorVoid
+import com.google.devtools.ksp.validate
+import dev.yamg.core.anotation.YamgItem
 
 class YamgItemSymbolProcessor(
     private val codeGenerator: CodeGenerator,
@@ -12,9 +16,33 @@ class YamgItemSymbolProcessor(
     private val options: Map<String, String>
 ) : SymbolProcessor {
 
+    var mappersPackageName: String = DEFAULT_MAPPERS_PACKAGE_NAME
 
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        TODO("Not yet implemented")
+        val symbols = resolver.getSymbolsWithAnnotation(
+            YamgItem::class.java.name
+        ).filterIsInstance<KSClassDeclaration>().distinct()
+        if (!symbols.iterator().hasNext()) return emptyList()
+
+        mappersPackageName = options[PARAM_MAPPER_PACKAGE_NAME] ?: DEFAULT_MAPPERS_PACKAGE_NAME
+
+        symbols.forEach { it.accept(YamgItemVisitor(resolver, logger), Unit) }
+        return symbols.filterNot { it.validate() }.toList()
     }
 
+    inner class YamgItemVisitor(
+        private val resolver: Resolver,
+        private val logger: KSPLogger
+    ) : KSVisitorVoid() {
+
+    }
+
+
+    companion object {
+
+        const val PARAM_MAPPER_PACKAGE_NAME = "mappersPackageName"
+
+        const val DEFAULT_EXTENSION_METHOD_NAME = "to%s"
+        const val DEFAULT_MAPPERS_PACKAGE_NAME = "dev.yamg.app"
+    }
 }
--- processor/src/main/kotlin/dev/yamg/processor/provider/YamgItemProcessorProvider.kt
+++ processor/src/main/kotlin/dev/yamg/processor/provider/YamgItemProcessorProvider.kt
@@ -3,6 +3,7 @@ package dev.yamg.processor.provider
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
+import dev.yamg.processor.processing.YamgItemSymbolProcessor
 
 class YamgItemProcessorProvider  : SymbolProcessorProvider {
 
