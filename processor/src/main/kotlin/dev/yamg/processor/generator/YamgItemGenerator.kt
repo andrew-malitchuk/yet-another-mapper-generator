21bfa2725335d7c6d10e1f941a4bf98a22dcfff9
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-02-21 19:51:05 +0200

    YAMG-8 Clean-up code

--- processor/src/main/kotlin/dev/yamg/processor/generator/YamgItemGenerator.kt
+++ processor/src/main/kotlin/dev/yamg/processor/generator/YamgItemGenerator.kt
@@ -6,18 +6,18 @@ import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.squareup.kotlinpoet.ClassName
 
 class YamgItemGenerator(
-private val classDeclaration: KSClassDeclaration,
-private val codeGenerator: CodeGenerator,
-private val methodName: String,
-private val fileName: String,
-private val parentClass: ClassName,
-private val className: ClassName,
-private val excludeFields: List<String>? = null,
-private val packageName: String,
-private val logger: KSPLogger
+    private val classDeclaration: KSClassDeclaration,
+    private val codeGenerator: CodeGenerator,
+    private val methodName: String,
+    private val fileName: String,
+    private val parentClass: ClassName,
+    private val className: ClassName,
+    private val excludeFields: List<String>? = null,
+    private val packageName: String,
+    private val logger: KSPLogger
 ) {
 
-    fun build(){
+    fun build() {
 
     }
 
--- processor/src/main/kotlin/dev/yamg/processor/provider/YamgItemProcessorProvider.kt
+++ processor/src/main/kotlin/dev/yamg/processor/provider/YamgItemProcessorProvider.kt
@@ -5,12 +5,12 @@ import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 import dev.yamg.processor.processing.YamgItemSymbolProcessor
 
-class YamgItemProcessorProvider  : SymbolProcessorProvider {
+class YamgItemProcessorProvider : SymbolProcessorProvider {
 
     override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor =
         YamgItemSymbolProcessor(
             codeGenerator = environment.codeGenerator,
-            options=environment.options,
+            options = environment.options,
             logger = environment.logger
         )
 
