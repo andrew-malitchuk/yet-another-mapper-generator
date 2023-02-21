b629c10595d35ef6360cddbc18d8eed93c8552b2
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-02-21 19:50:28 +0200

    YAMG-8 Add YamgItemGenerator

---
+++ processor/src/main/kotlin/dev/yamg/processor/generator/YamgItemGenerator.kt
@@ -0,0 +1,24 @@
+package dev.yamg.processor.generator
+
+import com.google.devtools.ksp.processing.CodeGenerator
+import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.squareup.kotlinpoet.ClassName
+
+class YamgItemGenerator(
+private val classDeclaration: KSClassDeclaration,
+private val codeGenerator: CodeGenerator,
+private val methodName: String,
+private val fileName: String,
+private val parentClass: ClassName,
+private val className: ClassName,
+private val excludeFields: List<String>? = null,
+private val packageName: String,
+private val logger: KSPLogger
+) {
+
+    fun build(){
+
+    }
+
+}
