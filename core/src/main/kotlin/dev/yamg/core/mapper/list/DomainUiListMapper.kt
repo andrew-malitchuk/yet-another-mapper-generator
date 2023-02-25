9d2ebf8f4d91aa09fad32f33eaf3aec5879aebcc
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-02-25 20:33:16 +0200

    YAMG-8 Change mapper hierarchy

--- core/src/main/kotlin/dev/yamg/core/mapper/list/DomainUiListMapper.kt
+++
@@ -1,8 +0,0 @@
-package dev.yamg.core.mapper.list
-
-import dev.yamg.core.model.DomainMapperModel
-import dev.yamg.core.model.UiMapperModel
-
-
-interface DomainUiListMapper<From : DomainMapperModel, To : UiMapperModel> :
-    BaseListMapper<From, To>
