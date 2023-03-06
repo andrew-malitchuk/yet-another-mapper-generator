b9ba46508d8f390522a6ec6525944b4d3c8a743b
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-03-06 19:01:53 +0200

    YAMG-9 Add YamgList annotation

---
+++ core/src/main/kotlin/dev/yamg/core/anotation/YamgList.kt
@@ -0,0 +1,20 @@
+package dev.yamg.core.anotation
+
+import dev.yamg.core.model.BaseMapperModel
+import kotlin.reflect.KClass
+
+
+@Target(AnnotationTarget.CLASS)
+annotation class YamgList(
+    val fromClass: KClass<out BaseMapperModel>,
+    val toClass: KClass<out BaseMapperModel>,
+    val className: String = "",
+){
+    companion object{
+        const val FROM_CLASS="fromClass"
+        const val TO_CLASS="toClass"
+        const val CLASS_NAME="className"
+        const val MAPPER_SUPPER_CLASS="mapperSupperClass"
+        const val EXCLUDE_FIELDS="excludeFields"
+    }
+}
